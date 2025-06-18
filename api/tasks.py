4fffb476f31e1b29f66071d82da294bb1cc10588
Sergey <Cthupby@gmail.com>
2025-06-18 11:30:22 +0300

    Update services naming

--- api/tasks.py
+++ api/tasks.py
@@ -1,12 +1,12 @@
 from config.celery import app
 
 from api.models import Car
-from api.services import add_cars, add_locations
+from api.services import add_car, add_location
 
 
 @app.task
 def start_api_task():
-    add_locations()
+    add_location()
     cars_count = Car.objects.count()
     if cars_count < 20:
-        add_cars()
+        add_car()
